4e2710877ccad8d9575833f98a0490744b3a51f9
dyestuff <axtemramazanov@mail.ru>
2026-05-27 17:29:58 +0300

    style: added Tailwind CSS support and starting defining Prisma ORM

---
+++ src/app/about/page.tsx
@@ -0,0 +1,5 @@
+import '@/app/globals.css'
+
+export default function Page() {
+  return <h1 className="flex justify-center">About Us Page</h1>
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
 export default function Page() {
   return (
+    <>
     <main className="flex flex-col items-center justify-center h-screen w-screen">
       Starting point
+      <h1 className=""></h1>
     </main>
+
+    </>
   );
 }
